9ee09903ed26dcbb9d5399d9659f13584cb06780
Kruithne <kruithne@gmail.com>
2013-05-04 18:15:14 +0100

    Fixed wrong co-ord set.

--- src/no/runsafe/peeveepeearena/pvpporter/TeleportEngine.java
+++ src/no/runsafe/peeveepeearena/pvpporter/TeleportEngine.java
@@ -20,12 +20,12 @@ public class TeleportEngine implements IConfigurationChanged
 		int lowZ = this.teleportPoint.getBlockZ() - this.teleportRadius;
 
 		newLocation.setX(this.getRandom(lowX, highX));
-		newLocation.setY(this.getRandom(lowZ, highZ));
+		newLocation.setZ(this.getRandom(lowZ, highZ));
 
 		while (!this.safeToTeleport(newLocation))
 		{
 			newLocation.setX(this.getRandom(lowX, highX));
-			newLocation.setY(this.getRandom(lowZ, highZ));
+			newLocation.setZ(this.getRandom(lowZ, highZ));
 		}
 
 		player.teleport(newLocation);
