b05241d93b71675fb2b0c8b9e5312d86be8ca8eb
Kruithne <kruithne@gmail.com>
2013-05-10 19:10:17 +0100

    Removed chest spawning from the plug-in for now.

--- src/no/runsafe/peeveepeearena/Plugin.java
+++ src/no/runsafe/peeveepeearena/Plugin.java
@@ -1,12 +1,6 @@
 package no.runsafe.peeveepeearena;
 
 import no.runsafe.framework.RunsafeConfigurablePlugin;
-import no.runsafe.peeveepeearena.chestdrop.ChestDropHandler;
-import no.runsafe.peeveepeearena.chestdrop.ChestDropRepository;
-import no.runsafe.peeveepeearena.chestdrop.LootTableManager;
-import no.runsafe.peeveepeearena.commands.CreateChestDrop;
-import no.runsafe.peeveepeearena.commands.ForcePvPChest;
-import no.runsafe.peeveepeearena.events.CloseInventory;
 import no.runsafe.peeveepeearena.events.RightClickBlock;
 import no.runsafe.peeveepeearena.events.SignChange;
 import no.runsafe.peeveepeearena.pvpporter.TeleportEngine;
@@ -21,18 +15,8 @@ public class Plugin extends RunsafeConfigurablePlugin
 		// Teleport
 		addComponent(TeleportEngine.class);
 
-		// Chest Drop
-		addComponent(ChestDropHandler.class); // Handler
-		addComponent(ChestDropRepository.class); // Repository
-		addComponent(LootTableManager.class);
-
 		// Events
-		addComponent(CloseInventory.class);
 		addComponent(SignChange.class);
 		addComponent(RightClickBlock.class);
-
-		// Commands
-		addComponent(CreateChestDrop.class);
-		addComponent(ForcePvPChest.class);
 	}
 }
