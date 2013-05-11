53cda5b6629ce64eaba680a10b2107f63348b70d
Kruithne <kruithne@gmail.com>
2013-05-11 07:15:21 +0100

    Fixed the complete and utter derp I made here.

--- src/no/runsafe/peeveepeearena/events/PlayerDropItems.java
+++ src/no/runsafe/peeveepeearena/events/PlayerDropItems.java
@@ -1,14 +1,30 @@
 package no.runsafe.peeveepeearena.events;
 
+import no.runsafe.framework.configuration.IConfiguration;
+import no.runsafe.framework.event.IConfigurationChanged;
 import no.runsafe.framework.event.player.IPlayerDropItemEvent;
 import no.runsafe.framework.server.event.player.RunsafePlayerDropItemEvent;
+import no.runsafe.framework.server.player.RunsafePlayer;
 
-public class PlayerDropItems implements IPlayerDropItemEvent
+public class PlayerDropItems implements IPlayerDropItemEvent, IConfigurationChanged
 {
 	@Override
 	public void OnPlayerDropItem(RunsafePlayerDropItemEvent event)
 	{
-		event.getPlayer().getInventory().remove(event.getItem().getItemStack());
-		event.setCancelled(true);
+		RunsafePlayer player = event.getPlayer();
+
+		if (player.getWorld().getName().equals(this.pvpWorld))
+		{
+			player.getInventory().remove(event.getItem().getItemStack());
+			event.setCancelled(true);
+		}
 	}
+
+	@Override
+	public void OnConfigurationChanged(IConfiguration configuration)
+	{
+		this.pvpWorld = configuration.getConfigValueAsString("pvpWorld");
+	}
+
+	private String pvpWorld;
 }
