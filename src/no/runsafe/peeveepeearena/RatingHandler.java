69cdd43ac7797c61a72636bb8c80de41f0e7ab33
Kruithne <kruithne@gmail.com>
2013-05-12 21:57:32 +0100

    Proper rounding/casting and using doubles for calculations.

--- src/no/runsafe/peeveepeearena/RatingHandler.java
+++ src/no/runsafe/peeveepeearena/RatingHandler.java
@@ -2,7 +2,6 @@ package no.runsafe.peeveepeearena;
 
 import no.runsafe.framework.configuration.IConfiguration;
 import no.runsafe.framework.event.IConfigurationChanged;
-import no.runsafe.framework.server.RunsafeServer;
 import no.runsafe.framework.server.player.RunsafePlayer;
 import no.runsafe.peeveepeearena.repositories.PlayerRatingRepository;
 
@@ -21,11 +20,9 @@ public class RatingHandler implements IConfigurationChanged
 		return this.repository.getRating(player);
 	}
 
-	private int getExpectedRating(int playerRating, int againstPlayerRating)
+	private double getExpectedRating(int playerRating, int againstPlayerRating)
 	{
-		int ea = 1 / (1 + 10 ^ ((againstPlayerRating - playerRating) / 400));
-		RunsafeServer.Instance.broadcastMessage("GER: " + ea);
-		return ea;
+		return 1 / (1 + 10 ^ ((againstPlayerRating - playerRating) / 400));
 	}
 
 	public List<Integer> getNewRating(RunsafePlayer winner, RunsafePlayer looser)
@@ -34,8 +31,8 @@ public class RatingHandler implements IConfigurationChanged
 		int winnerRating = this.getRating(winner);
 		int looserRating = this.getRating(looser);
 
-		int newWinnerRating = Math.round(winnerRating + this.kFactor * (1 - this.getExpectedRating(winnerRating, looserRating)));
-		int newLooserRating = Math.round(looserRating + this.kFactor * (0 - this.getExpectedRating(looserRating, winnerRating)));
+		int newWinnerRating = (int) Math.round(winnerRating + this.kFactor * (1 - this.getExpectedRating(winnerRating, looserRating)));
+		int newLooserRating = (int) Math.round(looserRating + this.kFactor * (0 - this.getExpectedRating(looserRating, winnerRating)));
 
 		ratings.add(0, Math.abs(newWinnerRating - winnerRating));
 		ratings.add(1, Math.abs(newLooserRating - looserRating));
