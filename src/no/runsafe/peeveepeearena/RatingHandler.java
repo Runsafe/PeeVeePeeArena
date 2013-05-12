21b29d748d508fdae753bf496d6789d6b28be68f
Kruithne <kruithne@gmail.com>
2013-05-12 22:02:34 +0100

    Added some debug in

--- src/no/runsafe/peeveepeearena/RatingHandler.java
+++ src/no/runsafe/peeveepeearena/RatingHandler.java
@@ -2,6 +2,7 @@ package no.runsafe.peeveepeearena;
 
 import no.runsafe.framework.configuration.IConfiguration;
 import no.runsafe.framework.event.IConfigurationChanged;
+import no.runsafe.framework.output.IOutput;
 import no.runsafe.framework.server.player.RunsafePlayer;
 import no.runsafe.peeveepeearena.repositories.PlayerRatingRepository;
 
@@ -10,9 +11,10 @@ import java.util.List;
 
 public class RatingHandler implements IConfigurationChanged
 {
-	public RatingHandler(PlayerRatingRepository repository)
+	public RatingHandler(PlayerRatingRepository repository, IOutput output)
 	{
 		this.repository = repository;
+		this.output = output;
 	}
 
 	public int getRating(RunsafePlayer player)
@@ -22,6 +24,7 @@ public class RatingHandler implements IConfigurationChanged
 
 	private double getExpectedRating(int playerRating, int againstPlayerRating)
 	{
+		this.output.broadcastColoured("Expected rating: " + 1 / (1 + 10 ^ ((againstPlayerRating - playerRating) / 400)));
 		return 1 / (1 + 10 ^ ((againstPlayerRating - playerRating) / 400));
 	}
 
@@ -31,8 +34,14 @@ public class RatingHandler implements IConfigurationChanged
 		int winnerRating = this.getRating(winner);
 		int looserRating = this.getRating(looser);
 
+		this.output.broadcastColoured("Current rating for " + winner.getName() + " = " + winnerRating);
+		this.output.broadcastColoured("Current rating for " + looser.getName() + " = " + looserRating);
+
 		int newWinnerRating = (int) Math.round(winnerRating + this.kFactor * (1 - this.getExpectedRating(winnerRating, looserRating)));
 		int newLooserRating = (int) Math.round(looserRating + this.kFactor * (0 - this.getExpectedRating(looserRating, winnerRating)));
+
+		this.output.broadcastColoured("New rating for " + winner.getName() + " = " + newWinnerRating);
+		this.output.broadcastColoured("New rating for " + looser.getName() + " = " + newLooserRating);
 
 		ratings.add(0, Math.abs(newWinnerRating - winnerRating));
 		ratings.add(1, Math.abs(newLooserRating - looserRating));
@@ -51,4 +60,5 @@ public class RatingHandler implements IConfigurationChanged
 
 	private PlayerRatingRepository repository;
 	private int kFactor;
+	private IOutput output;
 }
