b138e7d85d05670ba2f77bf4759906e113eb0db3
Kruithne <kruithne@gmail.com>
2013-05-31 21:21:02 +0100

    Check we're inside the PvP world before allowing the items to work

--- src/no/runsafe/peeveepeearena/PvPArenaEngine.java
+++ src/no/runsafe/peeveepeearena/PvPArenaEngine.java
@@ -1,8 +1,13 @@
 package no.runsafe.peeveepeearena;
 
+import no.runsafe.framework.configuration.IConfiguration;
+import no.runsafe.framework.event.IConfigurationChanged;
 import no.runsafe.framework.output.IOutput;
+import no.runsafe.framework.server.RunsafeServer;
+import no.runsafe.framework.server.RunsafeWorld;
+import no.runsafe.framework.server.player.RunsafePlayer;
 
-public class PvPArenaEngine
+public class PvPArenaEngine implements IConfigurationChanged
 {
 	public PvPArenaEngine(IOutput output)
 	{
@@ -14,5 +19,22 @@ public class PvPArenaEngine
 		this.output.broadcastColoured("&c[PvP] &f" + message);
 	}
 
+	public RunsafeWorld getPvPWorld()
+	{
+		return RunsafeServer.Instance.getWorld(this.pvpWorld);
+	}
+
+	public boolean isInPvPWorld(RunsafePlayer player)
+	{
+		return player.getWorld().getName().equals(this.pvpWorld);
+	}
+
+	@Override
+	public void OnConfigurationChanged(IConfiguration configuration)
+	{
+		this.pvpWorld = configuration.getConfigValueAsString("pvpWorld");
+	}
+
 	private IOutput output;
+	private String pvpWorld;
 }
--- src/no/runsafe/peeveepeearena/customitems/CustomItemHandler.java
+++ src/no/runsafe/peeveepeearena/customitems/CustomItemHandler.java
@@ -5,14 +5,16 @@ import no.runsafe.framework.server.block.RunsafeBlock;
 import no.runsafe.framework.server.item.RunsafeItemStack;
 import no.runsafe.framework.server.item.meta.RunsafeItemMeta;
 import no.runsafe.framework.server.player.RunsafePlayer;
+import no.runsafe.peeveepeearena.PvPArenaEngine;
 
 import java.util.HashMap;
 import java.util.Set;
 
 public class CustomItemHandler implements IPlayerRightClick
 {
-	public CustomItemHandler(ICustomItem[] customItems)
+	public CustomItemHandler(ICustomItem[] customItems, PvPArenaEngine pvpEngine)
 	{
+		this.pvpEngine = pvpEngine;
 		for (ICustomItem item : customItems)
 			this.customItems.put(item.getItemName().toLowerCase(), item);
 	}
@@ -20,7 +22,7 @@ public class CustomItemHandler implements IPlayerRightClick
 	@Override
 	public boolean OnPlayerRightClick(RunsafePlayer player, RunsafeItemStack usingItem, RunsafeBlock targetBlock)
 	{
-		if (usingItem != null)
+		if (usingItem != null && this.pvpEngine.isInPvPWorld(player))
 		{
 			RunsafeItemMeta meta = usingItem.getItemMeta();
 			if (meta != null)
@@ -62,4 +64,5 @@ public class CustomItemHandler implements IPlayerRightClick
 	}
 
 	private HashMap<String, ICustomItem> customItems = new HashMap<String, ICustomItem>();
+	private PvPArenaEngine pvpEngine;
 }
