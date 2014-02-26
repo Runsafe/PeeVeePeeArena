54b012c71467902e7d41b0002dad239b6c5ad68a
Morten Nilsen <morten@runsafe.no>
2014-02-26 17:49:58 +0100

    Kill player on correct thread. Fixes #3

--- src/no/runsafe/peeveepeearena/wilderness/DeathTriggers.java
+++ src/no/runsafe/peeveepeearena/wilderness/DeathTriggers.java
@@ -1,7 +1,7 @@
 package no.runsafe.peeveepeearena.wilderness;
 
 import no.runsafe.framework.api.IConfiguration;
-import no.runsafe.framework.api.IWorld;
+import no.runsafe.framework.api.IScheduler;
 import no.runsafe.framework.api.event.player.IPlayerCustomEvent;
 import no.runsafe.framework.api.event.plugin.IConfigurationChanged;
 import no.runsafe.framework.api.player.IPlayer;
@@ -13,6 +13,11 @@ import java.util.Map;
 
 public class DeathTriggers implements IConfigurationChanged, IPlayerCustomEvent
 {
+	public DeathTriggers(IScheduler scheduler)
+	{
+		this.scheduler = scheduler;
+	}
+
 	@Override
 	public void OnConfigurationChanged(IConfiguration config)
 	{
@@ -27,13 +32,20 @@ public class DeathTriggers implements IConfigurationChanged, IPlayerCustomEvent
 		if (event.getEvent().equals("region.enter"))
 		{
 			Map<String, String> data = (Map<String, String>) event.getData();
-			IPlayer player = event.getPlayer();
-
+			final IPlayer player = event.getPlayer();
 			if (deathRegions.contains(data.get("region")) && data.get("world").equals(this.wildernessWorld))
-				player.damage(100.0D);
+				scheduler.runNow(new Runnable()
+				{
+					@Override
+					public void run()
+					{
+						player.damage(100.0D);
+					}
+				});
 		}
 	}
 
 	private List<String> deathRegions = new ArrayList<String>();
 	private String wildernessWorld;
+	private final IScheduler scheduler;
 }
